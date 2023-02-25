3d2b9fd5b35f9c5303a9b8eff397ac14529220a9
kiriha1203 <mizumoto.takefumi@gmail.com>
2023-02-26 07:03:21 +0900

    taskController Response実装

--- src/app/Http/Controllers/TasksController.php
+++ src/app/Http/Controllers/TasksController.php
@@ -2,58 +2,113 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Task;
+
+use App\Http\Responses\ApiErrorResponse;
+use App\Http\Responses\ApiSuccessResponse;
+
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use OpenApi\Annotations as OA;
+use Throwable;
 
 class TasksController extends Controller
 {
     public function index()
     {
-        $tasks = Task::with('user:id,name')
-                        ->with('taskUsers:id,name')
-                            ->get();
-        return response()->json($tasks);
-    }
-
-    public function create(Request $request) {
-        $task = Task::create([
-            'title' => $request->title,
-            'context' => $request->context,
-            'status' => $request->status,
-            'create_user_id' => $request->create_user_id,
-        ]);
-        if ( !empty($request->assign_user_ids) ) {
-            $task->taskUsers()->attach($request->assign_user_ids);
+        try {
+            $tasks = Task::with('user:id,name')
+                            ->with('taskUsers:id,name')
+                                ->get();
+            return new ApiSuccessResponse(
+                ['tasks' => $tasks],
+                ['message' => 'Tasks get index successfully'],
+                Response::HTTP_OK
+            );
+        } catch (Throwable $exception) {
+            return new ApiErrorResponse(
+                'An error occurred while trying to get the tasks index',
+                $exception
+            );
         }
-
-        $responseTask = Task::with('user:id,name')
-                                ->with('taskUsers:id,name')
-                                    ->find($task->id);
-
-        return response()->json($responseTask);
     }
 
-    public function update(Request $request) {
-        $task = Task::with('user:id,name')
-                        ->with('taskUsers:id,name')
-                            ->find($request->task_id);
-        $task->update([
-            'title' => $request->title,
-            'context' => $request->context,
-            'status' => $request->status,
-        ]);
+    public function create(Request $request)
+    {
+        try {
+            $task = Task::create([
+                'title' => $request->title,
+                'context' => $request->context,
+                'status' => $request->status,
+                'create_user_id' => $request->create_user_id,
+            ]);
+            if (!empty($request->assign_user_ids)) {
+                $task->taskUsers()->attach($request->assign_user_ids);
+            }
+
+            $responseTask = Task::with('user:id,name')
+                                    ->with('taskUsers:id,name')
+                                        ->find($task->id);
+
+            return new ApiSuccessResponse(
+                ['task' => $responseTask],
+                ['message' => 'Task was created successfully'],
+                Response::HTTP_CREATED
+            );
+        } catch (Throwable $exception) {
+            return new ApiErrorResponse(
+                'An error occurred while trying to create the task',
+                $exception
+            );
+        }
     }
 
+    public function update(Request $request)
+    {
+        try {
+            $task = Task::with('user:id,name')
+                            ->with('taskUsers:id,name')
+                                ->find($request->task_id);
+            $task->update([
+                'title' => $request->title,
+                'context' => $request->context,
+                'status' => $request->status,
+            ]);
 
 
-    public function assignTask(Request $request) {
-        $task = Task::find($request->task_id);
-        $task->taskUsers()->sync($request->user_ids);
+            return new ApiSuccessResponse(
+                ['task' => $task],
+                ['message' => 'Task update successfully'],
+                Response::HTTP_OK
+            );
+        } catch (Throwable $exception) {
+            return new ApiErrorResponse(
+                'An error occurred while trying to update the task',
+                $exception
+            );
+        }
+    }
 
-        $responseTask = Task::with('user:id,name')
-                                ->with('taskUsers:id,name')
-                                    ->find($request->task_id);
+    public function assignTask(Request $request)
+    {
+        try {
+            $task = Task::find($request->task_id);
+            $task->taskUsers()->sync($request->user_ids);
 
-        return response()->json($responseTask);
+            $responseTask = Task::with('user:id,name')
+                                    ->with('taskUsers:id,name')
+                                        ->find($request->task_id);
+
+            return new ApiSuccessResponse(
+                ['task' => $responseTask],
+                ['message' => 'User update successfully'],
+                Response::HTTP_OK
+            );
+        } catch (Throwable $exception) {
+            return new ApiErrorResponse(
+                'An error occurred while trying to update the user',
+                $exception
+            );
+        }
     }
 }
